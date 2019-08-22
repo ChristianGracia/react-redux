548ee25323c6f23260be0bfa28af4b5c163318c8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-21 22:47:11 -0400

    reorganized

---
+++ React/React-Libraries/Bootstrap/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Test from "./Test";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test />
+      </div>
+    );
+  }
+}
+
+export default App;
