483011465be267e9fac86e1e6f69a5d0baaef2b5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-23 12:07:45 -0400

    added add todo hook

--- React/React-Hooks/todo/src/App.js
+++ React/React-Hooks/todo/src/App.js
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
+  return (
+    <div
+      className="todo"
+      style={{ fontWeight: "bold", fontSize: 20, width: "auto" }}
+    >
+      {todo.text}
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Add Todo..."
+      />
+    </form>
+  );
 }
 
 function App() {
@@ -23,13 +53,27 @@ function App() {
       isCompleted: false
     }
   ]);
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
-      <div className="todo-list">
+      <div
+        className="todo-list"
+        style={{
+          width: 300,
+          alignItems: "center",
+          paddingLeft: "25%",
+          paddingTop: 20
+        }}
+      >
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
