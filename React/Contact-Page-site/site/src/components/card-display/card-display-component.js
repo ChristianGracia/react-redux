90be446acdbf3218860596967983d2939e457929
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-14 02:45:46 -0400

    added onChange and turned carlist from a function to a class

--- React/Contact-Page-site/site/src/components/card-display/card-display-component.js
+++ React/Contact-Page-site/site/src/components/card-display/card-display-component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { CardList } from "../card-list/card-list-component";
+import CardList from "../card-list/card-list-component";
 
 class CardDisplay extends Component {
   render() {
--- React/Contact-Page-site/site/src/components/card-list/card-list-component.js
+++ React/Contact-Page-site/site/src/components/card-list/card-list-component.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "../card/card-component";
 import "./card-list.styles.css";
 
-export const CardList = props => {
-  return (
-    <div className="card-list">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
-};
+class CardList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.nativeEvent.target.value });
+    console.log(this.state.query);
+  }
+  render() {
+    return (
+      <div>
+        <h1>FriendFinder</h1>
+        <input
+          type="search"
+          placeholder="Search random 25"
+          onChange={this.handleChange}
+        />
+        <div style={{ padding: 10 }}></div>
+        <div className="card-list">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    );
+  }
+}
+export default CardList;
