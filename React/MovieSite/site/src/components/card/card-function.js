8dd2dfbca942b29697036b3568cbc92ed8f76cbd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 00:34:05 -0400

    movies displaying well now

--- React/MovieSite/site/src/components/card/card-function.js
+++ React/MovieSite/site/src/components/card/card-function.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import "./card.styles.css";
 
 export default function Card(props) {
   return (
-    <div>
+    <div class="card-list">
       {props.search.map(item => {
         return (
-          <div key={item.imdbID}>
+          <div key={item.imdbID} className="movies col-sm">
             <p>{item.Title}</p>
             <p>{item.Year}</p>
             <img src={item.Poster} />
-            <p>{item.Actors}</p>
+            <Button>
+              <p>hi</p>
+            </Button>
           </div>
         );
       })}
