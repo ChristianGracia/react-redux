94996dbd9788710bd743fdc99eee781174a25094
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 01:02:46 -0400

    added conditional rendering, set site up more

--- React/MovieSite/site/src/components/card-list/card-list-component.js
+++ React/MovieSite/site/src/components/card-list/card-list-component.js
@@ -2,36 +2,30 @@ import React, { Component } from "react";
 import Card from "../card/card-function";
 import "./card-list.styles.css";
 
+import Button from "react-bootstrap/Button";
+
 class CardList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: "",
-      movieArray: []
+      movieArray: [],
+      showHeader: true
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    // fetch(
-    //   "https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?s=dogs&apikey=791727ae"
-    // )
-    //   .then(data => data.json())
-    //   .then(data => {
-    //     var joined = this.state.movieArray.concat(data.Search);
-    //     this.setState({ movieArray: joined });
-    //   });
-  }
-
   handleClick(e) {
-    fetch(
-      `https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?s=${this.state.query}&apikey=791727ae`
-    )
+    var queryTitle = this.state.query.replace(/\s+/g, "%20");
+
+    var url =
+      "http://www.omdbapi.com/?s=%27" + queryTitle + "%27&apikey=791727ae";
+    fetch(url)
       .then(data => data.json())
       .then(data => {
         var joined = this.state.movieArray.concat(data.Search);
-        this.setState({ movieArray: joined });
+        this.setState({ movieArray: joined, showHeader: false });
       });
   }
 
@@ -41,15 +35,28 @@ class CardList extends Component {
   render() {
     return (
       <div>
-        <h1>Movie Search</h1>
+        {this.state.showHeader ? (
+          <div>
+            <h1 style={{ fontSize: 50, marginTop: "40vh" }}>Movie Search</h1>
+          </div>
+        ) : null}
+
+        <i
+          className="fas fa-search mr-2"
+          style={{ fontSize: 18, marginTop: 30 }}
+        ></i>
         <input
           type="search"
-          placeholder="Search movies"
+          placeholder="search movie database"
           onChange={this.handleChange}
           value={this.state.query}
         />
-        <input type="submit" onClick={this.handleClick} />
-        <div style={{ padding: 10 }}></div>
+
+        <Button className="ml-3" type="submit" onClick={this.handleClick}>
+          Search
+        </Button>
+
+        <div style={{ padding: 50 }}></div>
         <div className="card-list">
           <Card search={this.state.movieArray} />
         </div>
--- React/MovieSite/site/src/components/card/card-function.js
+++ React/MovieSite/site/src/components/card/card-function.js
@@ -4,15 +4,17 @@ import "./card.styles.css";
 
 export default function Card(props) {
   return (
-    <div class="card-list">
+    <div className="card-list">
       {props.search.map(item => {
         return (
-          <div key={item.imdbID} className="movies col-sm">
-            <p>{item.Title}</p>
-            <p>{item.Year}</p>
-            <img src={item.Poster} />
+          <div key={item.imdbID} className="col-sm">
+            <p>
+              {item.Title}, {item.Year}
+            </p>
+
+            <img className="movies" src={item.Poster} />
             <Button>
-              <p>hi</p>
+              <p>See More</p>
             </Button>
           </div>
         );
