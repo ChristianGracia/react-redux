df2cb7800917cbcfcf80d38782d9b1ade23e48ca
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-19 02:25:07 -0500

    update

--- React/sql/sql-db/src/Test.js
+++ React/sql/sql-db/src/Test.js
@@ -3,8 +3,22 @@ import React from "react";
 class Test extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      coinArr: []
+    };
   }
-  componentDidMount() {}
+  componentDidMount() {
+    fetch("http://localhost:8080/all")
+      .then(response => response.json())
+      .then(jsonData => {
+        // console.log(jsonData);
+        this.setState({ coinArr: jsonData });
+        console.log(this.state.coinArr);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
     return <div></div>;
