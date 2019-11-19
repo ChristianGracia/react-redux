5985a44c433c036004093b41e4b7ce00fc46f525
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-18 19:09:02 -0500

    added react app for working with sql

---
+++ React/sql/sql-db/src/Test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {}
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Test;
