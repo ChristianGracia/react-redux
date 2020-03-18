29c6e22c7bf96b21544d58f01a8af769f2608bbc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-17 21:51:26 -0400

    todo app working with hooks in typescript

--- React-Typescript/component-examples/interface-types-extends.tsx
+++ React-Typescript/component-examples/interface-types-extends.tsx
@@ -2,23 +2,29 @@ import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
 //interface creates a new type. extendable.
-interface: ITodo {
-  text: string,
-  complete: boolean
+interface ITodo {
+  text: string;
+  complete: boolean;
 }
 //extended
-interface: ITodo1 extends ITodo {
-  tags: string[]
+interface ITodo1 extends ITodo {
+  tags: string[];
 }
 //type refers to a type that already exists
 type formElem = React.FormEvent<HTMLFormElement>;
 export default function App(): JSX.Element {
   const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<Itodo[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const handleSubmit = (e: formElem): void => {
     e.preventDefault();
+    addTodo(value);
     setValue("");
+  };
+
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
   };
 
   return (
