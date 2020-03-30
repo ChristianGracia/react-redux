ba6f02eecba233525d6e64f8711c75e1729709d0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-29 22:36:06 -0400

    created episode list component

---
+++ React-Typescript/episode-picker/src/EpisodesList.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function EpisodesList(props: any): Array<JSX.Element> {
+  const { episodes, toggleFavAction, favorites } = props;
+  return <div>d</div>;
+}
