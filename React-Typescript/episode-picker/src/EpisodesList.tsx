67e10e464bd4facb3d0be13d111d55ded5d5d94a
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-29 22:39:21 -0400

    set up episode list code to receive episode data through props

--- React-Typescript/episode-picker/src/EpisodesList.tsx
+++ React-Typescript/episode-picker/src/EpisodesList.tsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { IEpisode } from "./interfaces/interfaces";
 
 export default function EpisodesList(props: any): Array<JSX.Element> {
   const { episodes, toggleFavAction, favorites } = props;
-  return <div>d</div>;
+
+  return episodes.map((episode: IEpisode) => {
+    return (
+      <section key={episode.id} className="episode-box">
+        <img
+          src={episode.image.medium}
+          alt={`Rick and morty ${episode.name}`}
+        />
+        <div>{episode.name}</div>
+        <section>
+          <div>
+            Season: {episode.season} Number: {episode.number}
+          </div>
+          <button type="button" onClick={() => toggleFavAction(episode)}>
+            {favorites.includes(episode) ? "Remove" : "Add"} Favorite
+          </button>{" "}
+        </section>
+      </section>
+    );
+  });
 }
